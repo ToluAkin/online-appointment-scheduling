e1353d4c925d7bcc5559a1866546af66ae3a2a4a
lorlahdey <horlorlahdey33@gmail.com>
2021-10-20 13:57:05 +0100

    updating Navlinks

--- client/src/views/components/Navbar.js
+++ client/src/views/components/Navbar.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { FlexboxGrid, Nav, Navbar } from "rsuite"
+import { routes } from "../../routes"
 import FlexboxGridItem from "rsuite/esm/FlexboxGrid/FlexboxGridItem"
 
 const NavbarMenu = ({ onSelect, activeKey}) => {
@@ -10,14 +11,14 @@ const NavbarMenu = ({ onSelect, activeKey}) => {
             <Navbar appearance='subtle' className='navbar-menu px-3 px-sm-5 py-2 mb-3'>
                 <FlexboxGrid justify="space-between" align='middle'>
                     <FlexboxGridItem>
-                        <Navbar.Brand className='navbar-menu-logo text-uppercase p-0' href='#'>oas</Navbar.Brand>
+                        <Navbar.Brand className='navbar-menu-logo text-uppercase p-0' href='/'>oas</Navbar.Brand>
                     </FlexboxGridItem>
                     <FlexboxGridItem className='d-flex align-items-center'>
                         {
                             isLogin
                             ?
                                 <Nav onSelect='' activeKey='' justified>
-                                    <Nav.Item className='navbar-menu-item me-4 p-0' eventKey='1'>Create Account</Nav.Item>
+                                    <Nav.Item className='navbar-menu-item me-4 p-0' eventKey='1' href={routes.SignUp}>Create Account</Nav.Item>
                                     <Nav.Item className='navbar-menu-item p-0' eventKey='2'>Login</Nav.Item>
                                 </Nav>
                             :
