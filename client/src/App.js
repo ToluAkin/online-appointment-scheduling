2650a886d1b064fbc5b6fdfc709361740aac15fd
ireti203 <opteaoged@gmail.com>
2021-10-21 13:41:21 +0100

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import { PublicRoutes } from './routes/publicRoutes';
 import Navbar from "./views/components/Navbar";
+import Login from "./views/pages/Login";
 
 function App() {
     return (
         <BrowserRouter>
             <Navbar />
+            <Login />
             <Switch>
                 { PublicRoutes.map((publicRoute, i) => {
                     return <Route  exact={ publicRoute.exact } Key={ i } path={ publicRoute.path } component={ publicRoute.component } />
--- client/src/views/pages/Homepage.js
+++ client/src/views/pages/Homepage.js
@@ -2,8 +2,7 @@ import {Button} from 'rsuite'
 
 const Homepage = () => {
     return (
-        <div>Home
-            <Button appearance="primary"> Hello world </Button>
+        <div>
         </div>
         
     )
